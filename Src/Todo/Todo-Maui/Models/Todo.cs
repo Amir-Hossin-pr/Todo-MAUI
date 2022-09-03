9738eeeab99586f4be74dc75d73cfd1c83042ee9
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-09-03 12:02:18 +0430

    create todo and todo view model

---
+++ Src/Todo/Todo-Maui/Models/Todo.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo_Maui.Models;
+
+public record Todo
+{
+    [PrimaryKey,AutoIncrement]
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+
+    public string Text { get; set; }
+}
---
+++ Src/Todo/Todo-Maui/ViewModels/TodoViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Todo_Maui.Models;
+
+namespace Todo_Maui.ViewModels;
+
+public class TodoViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private ObservableCollection<Todo> _todos;
+
+    public ObservableCollection<Todo> Todos
+    {
+        get
+        {
+            return _todos;
+        }
+        set
+        {
+            _todos = value;
+            OnPropertyChanged(nameof(Todos));
+
+        }
+    }
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+}
